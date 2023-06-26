e4a28554098ce4b65a48f144c8174c1e5f1386c3
Bruno Marchetti <107437629+Br1marchetti@users.noreply.github.com>
2023-06-26 17:32:38 -0300

    logitipo restaurante main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+#BRUNO MARCHETTI
+from pedido import Pedido, Quesadillas, Tacos, Tamales, Botanas, Burritos, Margarita, Gaseosa, Cerveza, Tequila, Agua
+from menu import Menu
+import psycopg2
 
+# Función para generar y mostrar el logotipo del menu
+def logo():
+    import time
+    time.sleep(0.1)
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    time.sleep(0.1)
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    time.sleep(0.1)
+    print("    Bienvenidos a ")
+    time.sleep(0.1)
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    time.sleep(0.1)
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    time.sleep(0.1)
+    print("         __   __   __   _  _   __   _           ")
+    time.sleep(0.1)
+    print("        |  __| |  _  | |  _  | |  \| | |  _  | | |          ")
+    time.sleep(0.1)
+    print("        | |    | || | | ||| | |\\ | | || | | |         ")
+    time.sleep(0.1)
+    print("        | |_  |  _  | |  _ \  | | \ | |  _  | | |__      ")
+    time.sleep(0.1)
+    print("        |_| || || || || || || || || |__|         by TEAM NOCHE")
+    time.sleep(0.1)
+    print("")
+    time.sleep(0.1)
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    time.sleep(0.1)
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    time.sleep(0.1)
+    print("                              Restaurante de comida mexicana.")
+    print("")
